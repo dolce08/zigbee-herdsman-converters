a9a868ce6fd39688b01161ad3dcadea53e7ac4f4
dolce08 <76213980+dolce08@users.noreply.github.com>
2025-04-24 12:44:54 +0100

    Update smartwings.ts
    Valid .mjs file but state is not reversed.

--- src/devices/smartwings.ts
+++ src/devices/smartwings.ts
@@ -1,15 +1,18 @@
-import * as fz from "../converters/fromZigbee";
-import * as tz from "../converters/toZigbee";
-import * as exposes from "../lib/exposes";
-import * as reporting from "../lib/reporting";
-import {assertString, getFromLookup, getOptions} from "../lib/utils";
-import type {DefinitionWithExtend, Tz, Zh} from "../lib/types";
+import * as fz from 'zigbee-herdsman-converters/converters/fromZigbee';
+import * as tz from 'zigbee-herdsman-converters/converters/toZigbee';
+import * as exposes from 'zigbee-herdsman-converters/lib/exposes';
+import * as reporting from 'zigbee-herdsman-converters/lib/reporting';
+import {assertString, getFromLookup, getOptions} from 'zigbee-herdsman-converters/lib/utils';
+
+/** @type{import('zigbee-herdsman-converters/lib/types').DefinitionWithExtend | import('zigbee-herdsman-converters/lib/types').DefinitionWithExtend[]} */
+/** @type{import('zigbee-herdsman-converters/lib/types').Tz} Tz | import('zigbee-herdsman-converters/lib/types').Tz} */
+/** @type{import('zigbee-herdsman-converters/lib/types').Zh} Zh | import('zigbee-herdsman-converters/lib/types').Zh} */
 
 const tzLocal = {
     backwards_cover_state: {
         key: ['state'],
-        convertSet: async (entity: Zh.Endpoint, key: string, value: number | string, meta: Tz.Meta) => {
-            const lookup = {open: 'downClose', close: 'upOpen', stop: 'stop', on: 'downClose', off: 'upOpen'};
+        convertSet: async (entity, key, value, meta) => {
+            const lookup = { open: 'downClose', close: 'upOpen', stop: 'stop', on: 'downClose', off: 'upOpen' };
             assertString(value, key);
             value = value.toLowerCase();
             await entity.command('closuresWindowCovering', getFromLookup(value, lookup), {}, getOptions(meta.mapped, entity));
@@ -18,20 +21,19 @@ const tzLocal = {
 };
 
 const e = exposes.presets;
-
-export const definitions: DefinitionWithExtend[] = [
+export default [
     {
-        zigbeeModel: ["WM25/L-Z"],
-        model: "WM25L-Z",
-        vendor: "Smartwings",
-        description: "Roller shade",
+        zigbeeModel: ['WM25/L-Z'],
+        model: 'WM25L-Z',
+        vendor: 'Smartwings',
+        description: 'Roller shade',
         fromZigbee: [fz.cover_position_tilt, fz.battery],
         toZigbee: [tzLocal.backwards_cover_state, tz.cover_position_tilt],
-        meta: {battery: {dontDividePercentage: true}, coverInverted: true},
+        meta: { battery: { dontDividePercentage: true }, coverInverted: true },
         configure: async (device, coordinatorEndpoint) => {
             const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ["genPowerCfg", "closuresWindowCovering"]);
-            device.powerSource = "Mains (single phase)";
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg', 'closuresWindowCovering']);
+            device.powerSource = 'Mains (single phase)';
             device.save();
         },
         exposes: [e.cover_position(), e.battery()],
